3dd4ef128eebf838933c252a9820e83c6c549e3c
LiuJie <1547698569@qq.com>
2025-01-05 15:15:44 +0800

    build: for release

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,7 +4,11 @@ export default defineConfig({
   test: {
     typecheck: {
       enabled: true,
-      tsconfig: './tsconfig.json'
+      tsconfig: './tsconfig.json',
+    },
+    coverage: {
+      enabled: true,
+      reporter: ['text', 'json', 'html'],
     },
     include: ['test/**/*.test-d.ts'],
   },
